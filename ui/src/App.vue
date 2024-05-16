f09f318ebd928b71ea03a63f48767402da6f4eb5
Yonfaaa <3064288605@qq.com>
2024-05-16 23:16:43 +0800

    V-Pro最终版

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -2,19 +2,15 @@
   <div id="app">
     <router-view />
     <theme-picker />
-<!--  <TestComponent/>-->
   </div>
 </template>
 
 <script>
 import ThemePicker from "@/components/ThemePicker";
-import TestComponent from "@/components/test.vue";
-import test from "@/components/test.vue";
-
 
 export default {
   name: "App",
-  components: {TestComponent, ThemePicker },
+  components: { ThemePicker },
   metaInfo() {
     return {
       title: this.$store.state.settings.dynamicTitle && this.$store.state.settings.title,
